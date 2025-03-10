442ea86ce9ef17385abd26e148757335060ea286
Musa Nazar <158774828+Musa-Nazar@users.noreply.github.com>
2025-03-10 10:14:19 +0000

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -13,39 +13,39 @@ function Counter({data,changeData,increment,decrement,reset}) {
       }>
       <div className={`
         ${CounterStyles.circle} 
-        border-${!data.darkMode ? "black" : "[#fff]"}`
+        border-${data.darkMode === false ? "black" : "[#fff]"}`
       }>
         <p className={`
           ${CounterStyles.count} 
           text-[${data.color}] 
-          text-${!data.darkMode ? "black" : "[#fff]"}`
+          text-${data.darkMode === false ? "black" : "[#fff]"}`
         }>{data.num}</p>
         <div className={`
           ${CounterStyles.line} 
-          border-${!data.darkMode ? "black" : "[#fff]"}`
+          border-${data.darkMode === false ? "black" : "[#fff]"}`
         }></div>
         <button className={`${CounterStyles.reset}`} onClick={reset}>Reset</button>
         <button className={`
         ${CounterStyles.buttons} 
-        text-${!data.darkMode ? "black" : "[#fff]"}`
+        text-${data.darkMode === false ? "black" : "[#fff]"}`
         } onClick={increment}>+</button>
         <button className={`
           ${CounterStyles.buttons} 
           left-[unset] right-[20%] 
-          text-${!data.darkMode ? "black" : "[#fff]"}`} onClick={decrement}>-</button>
+          text-${data.darkMode === false ? "black" : "[#fff]"}`} onClick={decrement}>-</button>
         <p className={`
         ${CounterStyles.smallCircle} 
         ${!data.showPop ? "hidden" : "block"}
-        text-${!data.darkMode ? "black" : "[#008000]"}
-        border-${!data.darkMode ? "black" : "[#fff]"}
+        text-${data.darkMode === false ? "black" : "[#008000]"}
+        border-${data.darkMode === false ? "black" : "[#fff]"}
         `}>{data.pop}</p>
       </div>
     </div>
     <div className={`${CounterStyles.range}`}>
       <input type="number" name="range" onChange={handleRange} className={`
       ${CounterStyles.rangeInput} 
-      text-${!data.darkMode ? "black" : "[#fff]"}
-      placeholder:text-${!data.darkMode ? "black" : "[#fff]"}
+      text-${data.darkMode === false ? "black" : "[#fff]"}
+      placeholder:text-${data.darkMode === false ? "black" : "[#fff]"}
       `} placeholder="Input Range"/>
     </div>
   </> 
